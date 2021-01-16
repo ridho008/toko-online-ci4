66166a4c91743d10a9a1b6491609ff746e070144
ridho008 <ciuyy12@gmail.com>
2021-01-16 23:13:13 +0700

    menambahkan constraint di table transaksi & komentar

--- app/Database/Migrations/2021-01-16-150024_Transaksi.php
+++ app/Database/Migrations/2021-01-16-150024_Transaksi.php
@@ -3,60 +3,57 @@
 use CodeIgniter\Database\Migration;
 class Transaksi extends Migration
 {
-	public function up()
-   {
-      $this->forge->addField([
-               'id'          => [
-                       'type'           => 'INT',
-                       'constraint'     => 5,
-                       'unsigned'       => true,
-                       'auto_increment' => true,
-               ],
-               'id_barang'       => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-                       'null' =>         true
-               ],
-               'id_pembeli' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-                       'null' =>         true
-               ],
-               'jumlah' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-               ],
-               'total_harga' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-               ],
-               'created_by' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-               ],
-               'created_date' => [
-                       'type'           => 'DATETIME',
-               ],
-               'updated_by' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-                       'null'             => true
-               ],
-               'updated_date' => [
-                       'type'           => 'DATETIME',
-                       'null'             => true
-               ],
-       ]);
-       $this->forge->addKey('id', true);
-       // $this->forge->addForeignKey('id_pembeli', 'user', 'id');
-       // $this->forge->addForeignKey('id_barang', 'barang', 'id');
-       $this->forge->createTable('transaksi');
-   }
+	public function up(){
+    $this->forge->addField([
+      'id'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+        'unsigned'=>TRUE,
+        'auto_increment'=>TRUE
+      ],
+      'id_barang'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+        'unsigned'=>TRUE,
+      ],
+      'id_pembeli'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+        'unsigned'=>TRUE,
+      ],
+      'jumlah'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+      ],
+      'total_harga'=>[
+        'type'=>'INT',
+        'constraint'=>11, 
+      ],
+      'created_by'=>[
+        'type' => 'INT',
+        'constraint' => 11,
+      ],
+      'created_date'=>[
+        'type' => 'DATETIME',
+      ],
+      'updated_by'=>[
+        'type' => 'INT',
+        'constraint' => 11,
+        'null' => TRUE,
+      ],
+      'updated_date'=>[
+        'type'=>'DATETIME',
+        'null'=>TRUE,
+      ]
+    ]);
 
-   //--------------------------------------------------------------------
+    $this->forge->addKey('id', TRUE);
+    $this->forge->addForeignKey('id_pembeli','user','id');
+    $this->forge->addForeignKey('id_barang','barang','id');
+    $this->forge->createTable('transaksi');
+  }
 
-   public function down()
-   {
-      $this->forge->dropTable('transaksi');
-   }
+  public function down(){
+    $this->forge->dropTable('transaksi');
+  }
 }
--- app/Database/Migrations/2021-01-16-150515_Komentar.php
+++ app/Database/Migrations/2021-01-16-150515_Komentar.php
@@ -4,55 +4,52 @@ use CodeIgniter\Database\Migration;
 
 class Komentar extends Migration
 {
-	public function up()
-   {
-      $this->forge->addField([
-               'id'          => [
-                       'type'           => 'INT',
-                       'constraint'     => 5,
-                       'unsigned'       => true,
-                       'auto_increment' => true,
-               ],
-               'id_barang'       => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-                       'null'         => true
-               ],
-               'id_user'       => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-                       'null'           => true
-               ],
-               'komentar' => [
-                       'type'           => 'TEXT',
-               ],
-               'created_by' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-               ],
-               'created_date' => [
-                       'type'           => 'DATETIME',
-               ],
-               'updated_by' => [
-                       'type'           => 'INT',
-                       'constraint'     => 11,
-                       'null'             => true
-               ],
-               'updated_date' => [
-                       'type'           => 'DATETIME',
-                       'null'             => true
-               ],
-       ]);
-       $this->forge->addKey('id', true);
-       // $this->forge->addForeignKey('id_user', 'user', 'id');
-       // $this->forge->addForeignKey('id_barang', 'barang', 'id');
-       $this->forge->createTable('komentar');
-   }
+	public function up(){
+    $this->forge->addField([
+      'id'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+        'unsigned'=>TRUE,
+        'auto_increment'=>TRUE
+      ],
+      'id_barang'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+        'unsigned'=>TRUE,
+      ],
+      'id_user'=>[
+        'type'=>'INT',
+        'constraint'=>11,
+        'unsigned'=>TRUE,
+      ],
+      'komentar'=>[
+        'type'=>'TEXT',
+      ],
+      'created_by'=>[
+        'type' => 'INT',
+        'constraint' => 11,
+      ],
+      'created_date'=>[
+        'type' => 'DATETIME',
+      ],
+      'updated_by'=>[
+        'type' => 'INT',
+        'constraint' => 11,
+        'null' => TRUE,
+      ],
+      'updated_date'=>[
+        'type'=>'DATETIME',
+        'null'=>TRUE,
+      ]
+    ]);
 
-   //--------------------------------------------------------------------
+    $this->forge->addKey('id', TRUE);
+    $this->forge->addForeignKey('id_user','user','id');
+    $this->forge->addForeignKey('id_barang','barang','id');
+    $this->forge->createTable('komentar');
+  }
 
-   public function down()
-   {
-      $this->forge->dropTable('komentar');
-   }
+  public function down(){
+    $this->forge->dropTable('komentar');
+  }
 }
